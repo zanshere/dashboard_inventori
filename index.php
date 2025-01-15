bce0762f81f03b4ea039cff54eff04422309d161
Fauzan <fauzan224321@gmail.com>
2025-01-15 23:49:52 +0700

    menambahkan text welcome

--- index.php
+++ index.php
@@ -3,12 +3,12 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Inventory Dashboard</title>
     <link rel="stylesheet" href="../dashboard_inventori/assets/CSS/index.css">
 
 </head>
 <body>
-    
+    <h1>welcome</h1>
 
 </body>
 </html>
