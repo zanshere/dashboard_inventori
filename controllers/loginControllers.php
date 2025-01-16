4160ee1c3b134a3fca8b7b39f29345b120b0f977
Fauzan <fauzan224321@gmail.com>
2025-01-16 14:55:06 +0700

    Menghapus title

--- controllers/loginControllers.php
+++ controllers/loginControllers.php
@@ -32,7 +32,6 @@ exit;
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
 </head>
 <body>
     <script type="text/javascript">
